726c72a686d79e9e7caf3df0ef49fb467a505bb5
YunSeon Kim <ysquty@naver.com>
2017-04-17 12:03:27 +0900

    first commit (SimpleCalc class)

---
+++ simpleCalc.java
@@ -0,0 +1,28 @@
+public class SimpleCalc{
+
+    int result;
+
+    public SimpleCalc(){
+        result = 0;
+    }
+
+    public void add(int x, int y){
+        result = x + y;
+    }
+
+    public void sub(int x, int y){
+        result = x - y;
+    }
+
+    public void div(int x, int y){
+        result = x / y;
+    }
+
+    public void multi(int x, int y){
+        result = x * y;
+    }
+
+    public int getResult(){
+        return result;
+    }
+}
